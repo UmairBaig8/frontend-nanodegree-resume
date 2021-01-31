0f288fd16c5a972faa4d5d884a05b1bff33265c2
Umair Baig <umairbaig808@gmail.com>
2021-01-31 20:53:32 +0530

    Try replce()

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,9 +2,9 @@
 This is empty on purpose! Your code to build the resume will go here.
  */
 
- var awesomethought = "I am Umair and I am awesome";
+ var awesomethought = "I am Umair and I am AWESOME";
  console.log(awesomethought);
 
 
- var funthought = awesomethought.replace("awesome", "fun");
+ var funthought = awesomethought.replace("AWESOME", "FUN");
  console.log(funthought);
