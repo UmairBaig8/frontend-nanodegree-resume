fb1676d1e682c5ef52cbb593466283ae21288b4f
Umair Baig <umairbaig808@gmail.com>
2021-01-31 19:56:30 +0530

    Try replace

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,10 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+ var awesomethought = "I am Umair and I am awesome";
+ console.log(awesomethought);
+
+
+ var funthought = awesomethought.replace("awesome", "fun");
+ console.log(funthought);
